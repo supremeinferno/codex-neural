19b504cf6096beb03bb45837d8a9feac566d0c79
Pranav Garg <317madhavgarg@gmail.com>
2026-09-08 20:29:33 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
-// export const API_URL =
-//   import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+export const API_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
-export const API_URL = "https://codex-neural.onrender.com";
